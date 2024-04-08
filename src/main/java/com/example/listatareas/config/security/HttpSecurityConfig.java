67127b35f501e783874e8b00994ec6ad558b0515
Belen Roman <124020263+BelenRomanCervantes@users.noreply.github.com>
2024-04-08 12:13:41 -0600

    Add Jwt auth filter and protect routes

--- src/main/java/com/example/listatareas/config/security/HttpSecurityConfig.java
+++ src/main/java/com/example/listatareas/config/security/HttpSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.example.listatareas.config.security;
 
+import com.example.listatareas.config.security.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +19,9 @@ public class HttpSecurityConfig {
     @Autowired
     private AuthenticationProvider authenticationProvider;
 
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -23,13 +29,15 @@ public class HttpSecurityConfig {
                 .csrf( csrfConfig -> csrfConfig.disable())
                 .sessionManagement( sessionMangConfig -> sessionMangConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authConfig -> {
 
-//                    authConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
-//                    authConfig.requestMatchers("/error").permitAll();
-//                    authConfig.requestMatchers("/api/v1/tareas").authenticated();
+                    authConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
+                    authConfig.requestMatchers("/error").permitAll();
+                    authConfig.requestMatchers("/api/v1/tareas/*").authenticated();
+                    authConfig.requestMatchers("/api/v1/usuarios/*").authenticated();
 
-                    authConfig.anyRequest().permitAll();
+                    authConfig.anyRequest().denyAll();
                 });
 
         return http.build();
--- src/main/java/com/example/listatareas/service/JwtService.java
+++ src/main/java/com/example/listatareas/service/JwtService.java
@@ -2,6 +2,7 @@ package com.example.listatareas.service;
 
 import com.example.listatareas.config.security.SecurityProperties;
 import com.example.listatareas.models.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.Map;
@@ -34,5 +36,18 @@ public class JwtService {
 
     private Key generateKey() {
         return Keys.hmacShaKeyFor(securityProperties.getSecretKey().getBytes());
+    }
+
+    public String extractUsername(String jwt) {
+        return extractAllClaims(jwt).getSubject();
+
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) generateKey())
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
     }
 }
